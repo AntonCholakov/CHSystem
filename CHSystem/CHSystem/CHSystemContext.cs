0d0483b1103403fa1429f4329e0b7f910bc965ea
Anita A <Anita A>
2015-11-21 16:15:34 +0200

    issue003-add EntityFramework, migrations set to automatic

---
+++ CHSystem/CHSystem/CHSystemContext.cs
@@ -0,0 +1,20 @@
+﻿using CHSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CHSystem
+{
+    public class CHSystemContext:DbContext
+    {
+        public CHSystemContext() : base("name=CHSystemDB") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Hall> Halls { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
