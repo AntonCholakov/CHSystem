15ce5ff43812c95890a128e77c2afb1c3e01c387
Vedi Mrt <Vedi Mrt>
2015-11-23 10:45:55 +0200

    issue006 - Add Repositories

---
+++ CHSystem/CHSystem/Repositories/EventRepository.cs
@@ -0,0 +1,12 @@
+﻿using CHSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHSystem.Repositories
+{
+    public class EventRepository:BaseRepository<Event>
+    {
+    }
+}
---
+++ CHSystem/CHSystem/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using CHSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHSystem.Repositories
+{
+    public class UserRepository:BaseRepository<User>
+    {
+        public User GetByUsernameAndPassword(string username, string password)
+        {
+            return First(u => u.Username == username && u.Password == password);
+        }
+    }
+}
