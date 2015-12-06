5f287e2180c60a049ee67c35132cfe42ff35219e
antoncholakov <anton_cholakov@ymail.com>
2015-12-06 13:44:13 +0200

    issue009 - Add Authentication

---
+++ CHSystem/CHSystem/ViewModels/Account/AccountLoginVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CHSystem.ViewModels.Account
+{
+    public class AccountLoginVM
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string RedirectUrl { get; set; }
+    }
+}
